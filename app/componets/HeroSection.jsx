3fb2212fc681c391526614029cd5528a19d8dfc0
sugutlynn <sugutlynn@gmail.com>
2023-10-03 23:19:04 +0300

    Hero Section

---
+++ app/componets/HeroSection.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const HeroSection = () => {
+    return ( 
+        <section>
+            <h1 className="text-white">Hello, I'm Lynn</h1>
+        </section>
+     );
+}
+ 
+export default HeroSection;
