3229fb4ba1bc5078358801c9ccb46ef24d4ebd4f
sugutlynn <sugutlynn@gmail.com>
2023-10-04 23:49:07 +0300

    Hero section

--- app/componets/HeroSection.jsx
+++ app/componets/HeroSection.jsx
@@ -4,7 +4,14 @@ import React from "react";
 const HeroSection = () => {
     return ( 
         <section>
-            <h1 className="text-white">Hello, I'm Lynn</h1>
+            <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Lynn</h1>
+            <p className="text-[#ADB7BE]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.
+
+            </p>
         </section>
      );
 }
