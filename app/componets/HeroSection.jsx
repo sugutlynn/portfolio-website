f1f3405d1e8e89d18819e7491db255a6e627fa9e
sugutlynn <sugutlynn@gmail.com>
2023-10-06 01:32:15 +0300

    update hero section

--- app/componets/HeroSection.jsx
+++ app/componets/HeroSection.jsx
@@ -1,35 +1,65 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client";
 import React from "react";
 import Image from "next/image";
 
-const HeroSection = () => {
-    return ( 
-        <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Lynn</h1>
-            <p className="text-[#ADB7BE] text-lg lg:text-4xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                molestiae quas vel sint commodi repudianda.
-            </p>
-                </div>
-            </div>
-            <div className="col-span-5">
-                <div className="rounded-full bg-[#181818]">
-                <Image
-                src="/images/mid-journey-image.jpg"
-                alt="hero image"
-                width={300}
-                height={300}
-                />
-                
+import { TypeAnimation } from "react-type-animation";
 
-                </div>
-               
-            </div>
-            
-        </section>
-     );
-}
- 
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+              Hello, I'm{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Lynn",
+                1000,
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "Game Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-4xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia.
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white hover:bg-slate-200">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[500px] h-[500px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/portfolio-image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default HeroSection;
--- app/page.js
+++ app/page.js
@@ -4,8 +4,11 @@ import HeroSection from './componets/HeroSection';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div class="container mx-auto px-12 py-4">
       <HeroSection/>
+      </div>
+     
     
     </main>
   )
