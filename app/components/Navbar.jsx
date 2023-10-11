d1f2ba4eb311f5d948eef4d4094a270ea8ca5b71
sugutlynn <sugutlynn@gmail.com>
2023-10-11 22:35:51 +0300

    MenuOverlay

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -33,11 +33,11 @@ const Navbar = () => {
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
-            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white">
+            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
-            <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white">
+            <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
               <XMarkIcon className="h-5 w-5" />
             </button>
           )}
