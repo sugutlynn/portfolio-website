7e8728420aa555122b1fd4eec262b36170722de7
sugutlynn <sugutlynn@gmail.com>
2023-10-13 19:47:27 +0300

    Project Section Data

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,6 +1,15 @@
-const ProjectCard = () => {
+import React from "react";
+
+const ProjectCard = ( { imgUrl, title, description}) => {
     return ( 
-        <div></div>
+        <div>
+            <div className="h-52 md:h-72" style={{background: `url(${imgUrl})`, backgroundSize: "cover"}}></div>
+            <div className="text-white">
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+        </div>
+        
      );
 }
  
--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,6 +1,66 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+    {
+      id: 1,
+      title: "React Portfolio Website",
+      description: "Project 1 description",
+      image: "/images/projects/1.png",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 2,
+      title: "Potography Portfolio Website",
+      description: "Project 2 description",
+      image: "/images/projects/2.png",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 3,
+      title: "E-commerce Application",
+      description: "Project 3 description",
+      image: "/images/projects/3.png",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 4,
+      title: "Food Ordering Application",
+      description: "Project 4 description",
+      image: "/images/projects/4.png",
+      tag: ["All", "Mobile"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 5,
+      title: "React Firebase Template",
+      description: "Authentication and CRUD operations",
+      image: "/images/projects/5.png",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 6,
+      title: "Full-stack Roadmap",
+      description: "Project 5 description",
+      image: "/images/projects/6.png",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+  ];
+
 const ProjectSection = () => {
     return ( 
-        <div>Project Section</div>
+        <h2>My Projects</h2>
      );
 }
  
