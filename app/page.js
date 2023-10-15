0d8167026ad70f9415db13a9a7ae34553af9016c
sugutlynn <sugutlynn@gmail.com>
2023-10-15 23:21:44 +0300

    Icon links

--- app/page.js
+++ app/page.js
@@ -1,20 +1,18 @@
-import Image from 'next/image';
-import HeroSection from './components/HeroSection';
-import Navbar from './components/Navbar';
-import AboutSection from './components/AboutSection';
-
-
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectSection from "./components/ProjectSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-      <HeroSection/>
-      <AboutSection/>
+        <HeroSection />
+        <AboutSection />
+        <ProjectSection />
       </div>
-     
-    
     </main>
-  )
+  );
 }
