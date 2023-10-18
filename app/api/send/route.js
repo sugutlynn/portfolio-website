4a82515b8b54367c909c78a0ec58f0ad08184afb
sugutlynn <sugutlynn@gmail.com>
2023-10-18 23:01:09 +0300

    fix error

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(re_ETKNTsHz_EX4UgUcQsCcw221GjsieVDbS);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
