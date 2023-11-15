360712d8ed6f004e759a92a9a928ed5b1e82deac
sugutlynn <sugutlynn@gmail.com>
2023-11-15 15:50:47 +0300

    rsesend api

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req, res) {
   const { body } = req;
