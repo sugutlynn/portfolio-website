6099b9d7f4c9506356fe0b90772d27095b72972e
sugutlynn <sugutlynn@gmail.com>
2023-11-20 20:34:47 +0300

    bg black on light mode

--- app/page.js
+++ app/page.js
@@ -9,9 +9,10 @@ import AchievementsSection from "./components/AchievementsSection"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <div className="bg-black">
+      <main className="flex min-h-screen flex-col">
       <Navbar />
-      <div className="container mt-24 px-12 py-4">
+      <div className=" mt-24 px-12 py-4">
         <HeroSection />
         <AchievementsSection />
         <AboutSection />
@@ -20,5 +21,7 @@ export default function Home() {
       </div>
       <Footer />
     </main>
+    </div>
+    
   );
 }
